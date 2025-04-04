edfb9ef39cc2c8e895ccda84087055c9a5f87c03
TenOClock-cs <nmartamonov@xmail.ru>
2025-04-04 15:59:42 +0300

    module33

---
+++ Module3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Module33()
+    {
+        string MyName = "Jane";
+        byte MyAge = 27;
+        bool HaveIApet = true;
+        double MyShoeSize = 37.5;
+
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
+    }
+    public static void Main(string[] args)
+    {
+        Module33();
+    }
+}
+
+        
