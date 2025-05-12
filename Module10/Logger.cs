fea4201cfac27f32d2d4b70a99e48cc723bcf27b
TenOClock-cs <nmartamonov@xmail.ru>
2025-05-12 13:43:04 +0300

    Module 10

---
+++ Module10/Logger.cs
@@ -0,0 +1,24 @@
+﻿namespace Module10
+{
+    internal interface ILogger
+    {
+        void LogError(string message);
+        void LogInfo(string message);
+    }
+    internal class Logger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Module10/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Module10;
+
+public interface ICalculator
+{
+    int Calculate(int x, int y);
+}
+
+public class Calculator : ICalculator
+{
+    public int Calculate(int x, int y)
+    {
+        return x + y;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        Logger logger = new Logger();
+        try
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            logger.LogInfo("Result = " + calculator.Calculate(a, b).ToString());
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+        }
+        finally
+        {
+            logger.LogInfo("Result calculated successfully");
+
+        }
+    }
+}
